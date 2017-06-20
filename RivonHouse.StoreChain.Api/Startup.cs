1dee21c2f20f951a92274fbff218ebf7628c8f56
Than Nguyen <rivon.programmer@gmail.com>
2017-06-21 00:54:41 +0700

    Register TodoContext

--- RivonHouse.StoreChain.Api/Startup.cs
+++ RivonHouse.StoreChain.Api/Startup.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using RivonHouse.StoreChain.Api.Models;
 
 namespace RivonHouse.StoreChain.Api
 {
@@ -27,6 +25,8 @@ namespace RivonHouse.StoreChain.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // Add dbContext
+            services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase());
             // Add framework services.
             services.AddMvc();
         }
